8d197413645fd8e5b325e080f2d98d88d5d7c2e7
Fkinds <Fkinds@gmail.com>
2025-11-01 03:31:22 +0900

    feat: improve commitlint

--- commitlint.config.js
+++ commitlint.config.js
@@ -1,3 +1,20 @@
 module.exports = {
   extends: ['@commitlint/config-conventional'],
+   "rules": {
+    "type-enum": [
+      2,
+      "always",
+      [
+        "feat",
+        "fix",
+        "refactor",
+        "test",
+        "chore",
+      ]
+    ],
+    "subject-case": [2, "never", ["sentence-case", "start-case", "pascal-case", "upper-case"]],
+    "header-max-length": [2, "always", 100],
+    "body-leading-blank": [1, "always"],
+    "footer-leading-blank": [1, "always"]
+  }
 };
